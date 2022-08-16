9d6c1c12dc27aa676431991416f47bf918866954
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 13:19:13 +0530

    declared and initilaised no argument constructor with initial values

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -7,4 +7,12 @@ public class Customer {
     private long mobileNo;
     private String email;
     private boolean premiumMembership;
+
+    Customer(){
+        name = null;
+        address = null;
+        mobileNo = 0;
+        email = null;
+        premiumMembership = false;
+    }
 }
