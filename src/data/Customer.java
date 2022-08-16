59a35e205ecbd8b6013f9a0b4896d4507cec5cf8
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 13:15:08 +0530

    declared attributes

---
+++ src/data/Customer.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Customer {
+
+    private String name;
+    private String address;
+    private long mobileNo;
+    private String email;
+    private boolean premiumMembership;
+}
