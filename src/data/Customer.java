1a4ddd3d9fad1002f9da9e3ff9c61206c74758ff
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 13:21:37 +0530

    declared and initilaised parameterised constructor

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -15,4 +15,12 @@ public class Customer {
         email = null;
         premiumMembership = false;
     }
+
+    Customer(String name,String address,long mobileNo,String email,boolean premiumMembership){
+        this.name = name;
+        this.address = address;
+        this.mobileNo = mobileNo;
+        this.email = email;
+        this.premiumMembership = premiumMembership;
+    }
 }
